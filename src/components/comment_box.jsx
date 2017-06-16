25970c979250c2f483780a22fc15e04faada750e
Aaron Gottlieb <atticus19@gmail.com>
2017-06-16 15:48:20 -0600

    adds test for comment box component

---
+++ src/components/comment_box.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class CommentBoxt extends Component {
+  render() {
+    return (
+      <div className="comment-box">
+        <textarea name="" id="" cols="30" rows="10" />
+        <button>Submit Comment</button>
+
+      </div>
+    )
+  }
+}
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -6,8 +6,10 @@ describe('App', () => {
 
   // Use 'it' to test a single attribute of a target
   it('shows the correct text', () => {
+
     //create an instance of app
     const component = renderComponent(App);
+
     // Use 'expect' to make an 'assertion' about a target
     expect(component).to.contain('React simple starter');
 
