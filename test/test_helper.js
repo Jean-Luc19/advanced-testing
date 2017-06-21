2bc463fa40efab082187fdc4c0288fffe3734cdf
Aaron Gottlieb <atticus19@gmail.com>
2017-06-21 16:46:01 -0600

    builds test-helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,8 @@
 import jsdom from 'jsdom';
 
-// Set up testing environment to run like a browser in the cli
+// Set up testing environment to run like a browser in the CLI
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>'); //equivalent to window.document
+global.window = global.document.defaultView;
 
 // Build renderComponent element that should render a given react Class
 
